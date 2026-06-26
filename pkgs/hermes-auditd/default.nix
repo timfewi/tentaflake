ad2ae209e16e6828193e5abda4a40259bacaec64
timfewi <123400131+timfewi@users.noreply.github.com>
2026-06-26 23:12:46 +0200

    release: v0.1.1

--- pkgs/hermes-auditd/default.nix
+++ pkgs/hermes-auditd/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule {
   pname = "hermes-auditd";
-  version = "0.1.0";
+  version = "0.1.1";
   src = ./.;
   vendorHash = "sha256-+b+JmOmyut/bhwQSsmv49fpRfi4cW1qxrQoDbs9CMBE=";
   subPackages = [
